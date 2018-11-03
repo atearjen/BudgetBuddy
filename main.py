75618a88163be0d7e8bf72cedbd10b73390530fa
nwon4ever <nwon4ever@gmail.com>
2018-11-03 11:37:14 -0700

    main

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from bmonth import bmonth
+from flask import Flask, render_template, request
+from flask_cors import CORS
 
 
-test = bmonth()
+#test = bmonth()
 
 #numDays=test.init(1,1,2018,2,1,2018,1500)
 #budgPerDay=test.getBudget(1500,numDays)
@@ -13,4 +15,8 @@ test = bmonth()
 #print(test.getDays())
 #print(test.getBudget())
 
-print test.entertainment(.15)
+#print test.entertainment(.15)
+
+#####################################################################################
+app = Flask(__name__)
+CORS(app)
