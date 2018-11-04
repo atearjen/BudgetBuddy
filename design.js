cb67b4f8eb9965976c709f2371747e2901c10c46
Allison <amtearjen@ucdavis.edu>
2018-11-04 00:12:35 -0700

    Here comes the front :)

--- design.js
+++ design.js
@@ -1,5 +1,5 @@
 
-var API_BASE_URL = 'https://localhost:5005'
+var API_BASE_URL = 'http://localhost:5005'
 
 function sendGet(route, cb) {
   var url = API_BASE_URL + route;
@@ -24,7 +24,6 @@ function sendPost(route, d, cb) {
   });
 }
 
-
 var $form = $("#data");
 
 var $button = $('a');
@@ -32,9 +31,6 @@ $form.on('submit', function(event) {
   event.preventDefault();
   var duration = 0.3,
       delay = 0.08;
-  // TweenMax.to($button, duration, {scaleY: 1.6, ease: Expo.easeOut});
-  // TweenMax.to($button, duration, {scaleX: 1.2, scaleY: 1, ease: Back.easeOut, easeParams: [3], delay: delay});
-  // TweenMax.to($button, duration * 1.25, {scaleX: 1, scaleY: 1, ease: Back.easeOut, easeParams: [6], delay: delay * 3 });
 
   console.log($form.serializeArray());
   sendPost("/save",$form.serializeArray(),function () {
@@ -45,12 +41,11 @@ $form.on('submit', function(event) {
   // })
 
   //console.log($form.serializeArray());
-  $.post("/save",$form.serializeArray(),function () {
-    console.log("got here");
-  });
-  $.get("/item",function (data) {
-    console.log(data);
-  })
+  // $.post("/save",$form.serializeArray(),function () {
+  //   console.log("got here");
+  // });
+
+
 
 });
 
